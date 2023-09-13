9af8be9edc7af1167b9f1fb00c321b57bb3dcd5e
Chigozie Mbama Mykel <92220456+mbamamykel77@users.noreply.github.com>
2023-09-13 14:05:05 +0100

    route edit

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 import {createUser, getUser, updateUser, deleteUser} from "../controllers/user.js";
 
-router.route("/").post(createUser);
-router.route("/").get(getUser).put(updateUser).delete(deleteUser)
+router.route("/").post(createUser).get(getUser);
+router.route("/:id").put(updateUser).delete(deleteUser)
 
 
 
